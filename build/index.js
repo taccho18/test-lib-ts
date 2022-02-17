7873132cf22061c2f669af3b3aa86a38d85f097d
taccho18 <momoka.takahashi@ntt.com>
2022-02-17 16:41:43 +0900

    Add build

---
+++ build/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    call() {
+        return this.name;
+    }
+    testPerson() {
+        return "test";
+    }
+}
+exports.Person = Person;
+//# sourceMappingURL=index.js.map
